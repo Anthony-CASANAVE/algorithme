2138c7554f4cdf9df9eb2cad3df65ebc7e17f2c0
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-11-29 14:17:34 +0100

    Fonction de calcule de périmètre ajoutée.

---
+++ script/index.js
@@ -0,0 +1,6 @@
+let height = parseInt(prompt("Entrez une hauteur"));
+console.log("Hauteur entrée : " + height);
+let weight = parseInt(prompt("Entrez une largeur"));
+console.log("Largeur entrée : " + weight);
+let perimeter = height * weight;
+console.log("Le périmètre de votre rectangle est : " + perimeter);
