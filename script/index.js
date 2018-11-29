58fa0d92cd1dcfcee80df38dcff6d08bd21c350a
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-11-29 14:25:51 +0100

    Amélioration de l'affichage du calcule du périmètre.

--- script/index.js
+++ script/index.js
@@ -2,5 +2,15 @@ let height = parseInt(prompt("Entrez une hauteur"));
 console.log("Hauteur entrée : " + height);
 let weight = parseInt(prompt("Entrez une largeur"));
 console.log("Largeur entrée : " + weight);
-let perimeter = height * weight;
-console.log("Le périmètre de votre rectangle est : " + perimeter);
+let perimeter = (height * 2) + (weight * 2);
+console.log("Le périmètre de votre rectangle est : " + "2" + "*" + height + " + " + "2" + "*" + weight + " = " + (height * 2) + "+" + (weight * 2) + " = " + perimeter);
+
+
+
+
+
+
+
+
+
+
